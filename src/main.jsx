28abaa524fdaeb1b16bcad0cbc0a8323dc87b4b7
JosedLeonett <josed.95@gmail.com>
2023-07-08 12:57:03 -0300

    Migrate <BrowserRouter> to <RouterProvider/<
    react- router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
 import { ContextProvider } from './Components/utils/global.context';
 import App from './App';
 import Home from "./Routes/Home";
@@ -10,11 +10,48 @@ import Detail from "./Routes/Detail";
 import Favs from "./Routes/Favs";
 import NotFound from "./Routes/NotFound";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    //Colocar Loader despues.
+    children: [
+      {
+        index: true,
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "contact",
+        element: <Contact/>
+      },
+      {
+        path: "dentist/:id",
+        element: <Detail />
+      },
+      {
+        path: "favs",
+        element: <Favs />
+      },
+      {
+        path: "*",
+        element: <NotFound />
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+
+
+
+
+    {/* <BrowserRouter> */}
       <ContextProvider>
-        <Routes>
+        {/* <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Home />} />
             <Route path="contact" element={<Contact />} />
@@ -22,8 +59,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route path="favs" element={<Favs />} />
             <Route path="*" element={<NotFound />} />
           </Route>
-        </Routes>
+        </Routes> */}
+
+        <RouterProvider router={router} />
+
       </ContextProvider>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
